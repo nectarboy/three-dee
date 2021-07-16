df3971aa66d0358fe05ea5c819742989d58dc5aa
nectarboy <59830504+nectarboy@users.noreply.github.com>
2021-07-16 19:12:20 +0300

    [triangle_rasterizer] prolly doing this wrong TwT

--- rasterizer/triangle/triangle_rasterizer.js
+++ rasterizer/triangle/triangle_rasterizer.js
@@ -8,6 +8,7 @@ const CANV_H = 256;
 
 const COL_FG = 0xffff66;
 const COL_BG = 0x222222;
+const COL_DEBUG = 0xff0000;
 
 // --- Canvas
 const canv = document.getElementById('canv');
@@ -48,31 +49,44 @@ function rasterizeVerts(verts) {
     verts[1].x |= 0; verts[1].y |= 0;
     verts[2].x |= 0; verts[2].y |= 0;
 
-    // Find height of rasterization
-    var upmost = 0;
-    var downmost = 0;
+    // Find points of rasterization
+    var top = 0;
+    var bot = 0;
     switch (Math.min(verts[0].y, verts[1].y, verts[2].y)) {
-        case verts[0].y: upmost = 0; break;
-        case verts[1].y: upmost = 1; break;
-        case verts[2].y: upmost = 2; break;
+        case verts[0].y: top = 0; break;
+        case verts[1].y: top = 1; break;
+        case verts[2].y: top = 2; break;
     }
     switch (Math.max(verts[0].y, verts[1].y, verts[2].y)) {
-        case verts[0].y: downmost = 0; break;
-        case verts[1].y: downmost = 1; break;
-        case verts[2].y: downmost = 2; break;
+        case verts[0].y: bot = 0; break;
+        case verts[1].y: bot = 1; break;
+        case verts[2].y: bot = 2; break;
     }
 
-    var midmost = ((((upmost+1) ^ (downmost+1)) - 1) & 3) % 3; // Ugly ass formula i came up with :>
+    var mid = ((((top+1) ^ (bot+1)) - 1) >>> 0) % 3; // Ugly ass formula i came up with :>
+    console.log(verts[top].y, verts[mid].y, verts[bot].y); // Should be in (lo-mid-hi) order !
 
-    console.log(verts[upmost].y, verts[midmost].y, verts[downmost].y);
-    // Draw vertices
-    for (var i = 0; i < 3; i++)
-        putPx(verts[i].x, verts[i].y, COL_FG);
+    // Extra properties ...
+    const slopeTB = ((CANV_H - verts[bot].y) - (CANV_H - verts[top].y)) / ((CANV_W - verts[bot].x) - (CANV_W - verts[top].x));
+    console.log(slopeTB);
 
     // Rasterize
-    for (var i = verts[upmost].y; i < verts[downmost].y; i++) {
+    for (var i = 0; i < verts[bot].y - verts[top].y; i++) {
+        const y = verts[top].y + i;
+        const belowMid = (y > verts[mid].y);
 
+        var start = 0;
+        var width = 10;
+
+        start = verts[top].x - (i * slopeTB);
+
+        for (var ii = 0; ii < width; ii++)
+            putPx(start + ii, y, COL_FG);
     }
+
+    // Debug vertices
+    for (var i = 0; i < 3; i++)
+        putPx(verts[i].x, verts[i].y, COL_DEBUG);
 }
 
 // --- Execution
