e15a1af72d1fae8668f12f06ca5da59220d829a6
nectarboy <59830504+nectarboy@users.noreply.github.com>
2021-07-18 15:28:38 +0300

    [triangle_rasterizer] functional (flat color) !
    it looks crusty in certain cases but it works nonetheless :)
    i might refactor the entire project boilerplate

--- rasterizer/triangle/triangle_rasterizer.js
+++ rasterizer/triangle/triangle_rasterizer.js
@@ -6,7 +6,7 @@
 const CANV_W = 256;
 const CANV_H = 256;
 
-const COL_FG = 0xffff66;
+const COL_FG = 0x00ff00;
 const COL_BG = 0x222222;
 const COL_DEBUG = 0xff0000;
 
@@ -37,19 +37,22 @@ function clearBg() {
             putPx(i, ii, COL_BG);
 }
 
-function render() {
+function renderScene() {
     ctx.putImageData(buffer, 0, 0);
 }
 
 // --- Rasterization
 // @param 'vertices' is an Array full of Objects containing 'x' and 'y' coordinates in Integers.
 function rasterizeVerts(verts) {
-    // Convert vertices to ints
+    if (verts.length !== 3)
+        throw 'rasterizeVerts :: invalid \'verts\' array !';
+
+    // Convert vertices to ints for the naughty boys
     verts[0].x |= 0; verts[0].y |= 0;
     verts[1].x |= 0; verts[1].y |= 0;
     verts[2].x |= 0; verts[2].y |= 0;
 
-    // Find points of rasterization
+    // Find main points of the vertices
     var top = 0;
     var bot = 0;
     switch (Math.min(verts[0].y, verts[1].y, verts[2].y)) {
@@ -63,22 +66,36 @@ function rasterizeVerts(verts) {
         case verts[2].y: bot = 2; break;
     }
 
-    var mid = ((((top+1) ^ (bot+1)) - 1) >>> 0) % 3; // Ugly ass formula i came up with :>
-    console.log(verts[top].y, verts[mid].y, verts[bot].y); // Should be in (lo-mid-hi) order !
+    const mid = ((((top+1) ^ (bot+1)) - 1) >>> 0) % 3; // Ugly ass formula i came up with :>
+    // console.log(verts[top].y, verts[mid].y, verts[bot].y); // Should be in (lo-mid-hi) order !
 
     // Extra properties ...
-    const slopeTB = ((CANV_H - verts[bot].y) - (CANV_H - verts[top].y)) / ((CANV_W - verts[bot].x) - (CANV_W - verts[top].x));
-    console.log(slopeTB);
+    const slopeTB = (-verts[bot].y + verts[top].y) / (verts[bot].x - verts[top].x);
+    const slopeTM = (-verts[mid].y + verts[top].y) / (verts[mid].x - verts[top].x);
+    const slopeBM = (-verts[mid].y + verts[bot].y) / (verts[mid].x - verts[bot].x);
+    // console.log(slopeTB, slopeTM, slopeBM);
 
-    // Rasterize
-    for (var i = 0; i < verts[bot].y - verts[top].y; i++) {
+    // Rasterize from top to bottom
+    const height = (verts[bot].y - verts[top].y)+1;
+    for (var i = 0; i < height; i++) {
         const y = verts[top].y + i;
-        const belowMid = (y > verts[mid].y);
 
-        var start = 0;
-        var width = 10;
+        // Calculating line
+        const start = verts[top].x - (i * (1/slopeTB));
+        const width = Math.round((y > verts[mid].y) // (Are we below the middle vert ?)
+            ? (verts[mid].x + ((height - i) * (1/slopeBM))) - start - (verts[mid].x - verts[bot].x)
+            : (verts[mid].x - (i * (1/slopeTM))) - start - (verts[mid].x - verts[top].x)
+        );
 
-        start = verts[top].x - (i * slopeTB);
+        // Drawing line
+        if (width < 0) {
+            for (var ii = -width; ii >= 0; ii--)
+                putPx(start - ii, y, COL_FG);
+        }
+        else {
+            for (var ii = 0; ii < width; ii++)
+                putPx(start + ii, y, COL_FG);
+        }
 
         for (var ii = 0; ii < width; ii++)
             putPx(start + ii, y, COL_FG);
@@ -90,18 +107,57 @@ function rasterizeVerts(verts) {
 }
 
 // --- Execution
+function resetScene() {
+    clearBg();
+    renderScene();
+}
+resetScene();
+
 const defaultVerts = [
-    {x: 10, y: 200},
-    {x: 150, y: 10},
-    {x: 200, y: 150}
+    // [
+    //     {x: 10, y: 200},
+    //     {x: 150, y: 10},
+    //     {x: 200, y: 150}
+    // ],
+
+    // [
+    //     {x: 20, y: 10},
+    //     {x: 20, y: 20},
+    //     {x: 10, y: 20},
+    // ],
+    // [
+    //     {x: 30, y: 10},
+    //     {x: 30, y: 20},
+    //     {x: 40, y: 20},
+    // ],
+    // [
+    //     {x: 10, y: 30},
+    //     {x: 40, y: 30},
+    //     {x: (10+40)/2, y: 40},
+    // ],
+
+    [
+        {x: 0, y: CANV_H/2},
+        {x: CANV_W/2, y: 0},
+        {x: CANV_W, y: CANV_H/2},
+    ],
+    [
+        {x: 0, y: CANV_H/2},
+        {x: CANV_W, y: CANV_H/2},
+        {x: CANV_W/2, y: CANV_H},
+    ],
 ];
 
-function init() {
-    clearBg();
-    render();
-}
+setInterval(() => {
+    defaultVerts[0][1].y++;
+    defaultVerts[1][2].y--;
 
-init();
+    defaultVerts[0][0].x++;
+    defaultVerts[0][2].x--;
+    defaultVerts[1][0].x++;
+    defaultVerts[1][1].x--;
 
-rasterizeVerts(defaultVerts);
-render();
+    resetScene();
+    defaultVerts.forEach(v => rasterizeVerts(v));
+    renderScene();
+}, 1000/60);
